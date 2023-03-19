8495d5af4d66d684633d613e57ecfc607a70e899
huhuiwen <1050615501@qq.com>
2023-03-19 13:42:38 -0500

    add oper

--- src/main/java/com/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/thymeleafdemo/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -17,7 +19,6 @@ public class EmployeeController {
 	private EmployeeService employeeService;
 	public EmployeeController(EmployeeService theEmployeeService) {
 		employeeService = theEmployeeService;
-
 	}
 
 	@GetMapping("/list")
@@ -28,7 +29,24 @@ public class EmployeeController {
 		// add to the spring model
 		theModel.addAttribute("employees", theEmployees);
 
-		return "list-employees";
+		return "employees/list-employees";
+	}
+
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model theModel) {
+		Employee theEmployee = new Employee();
+
+		theModel.addAttribute("employee", theEmployee);
+		return "employees/employee-form";
+	}
+
+	@PostMapping("/save")
+	public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
+		// save the employee
+		employeeService.save(theEmployee);
+
+		// use a redirect to prevent duplicate submission
+		return "redirect:/employees/list";
 	}
 }
 
